84f59395cfe7aa121b8a1c9136e2802c70adccb2
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-05 22:11:39 +0530

    App component

---
+++ pract/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-2xl bg-slate-500 text-center w-full h-min p-3">
+        <h3>TODO APP</h3>
+    </div>
+    </>
+  )
+}
+
+export default App
