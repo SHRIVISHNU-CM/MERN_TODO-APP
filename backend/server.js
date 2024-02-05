522dafddc7325c26d5976d6bd9921acb9d3f4a53
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-05 20:48:38 +0530

    adding router

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const database = require('./database/connection')
+const database = require('./database/connection');
+const router = require('./routes/router')
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT||5002;
-
+app.use('/api/',router)
 
 app.listen(PORT,()=>console.log(`Server is runnning at ${PORT}`))
