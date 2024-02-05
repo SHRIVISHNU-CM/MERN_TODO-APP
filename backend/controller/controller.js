8ccb08e5665b075967bef51ccd559040f4d2496f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-05 20:53:06 +0530

    controller for todo

---
+++ backend/controller/controller.js
@@ -0,0 +1,40 @@
+const todo = require("../database/databaseSchema")
+
+module.exports.getItems = async(req,res)=>{
+    const item = await todo.find()
+    res.send(item)
+}
+
+module.exports.saveItems = (req,res)=>{
+    const { todoItems }= req.body;
+    todo.create({ todoItems })
+    .then((data)=>{
+        console.log(data)
+        res.status(201).json({"Todo Item Created":data});
+        })
+        .catch((err)=> {
+            console.log(err);
+            res.status(500).json({error: err});
+            });
+}
+
+module.exports.updateItems = (req,res)=>{
+    const id = req.params.id;
+    const {todoItems} = req.body;
+    todo.findByIdAndUpdate(id, {todoItems})
+    .then((data)=>{
+        console.log("Updated successfully")
+        res.status(201).json(data)
+    })
+    .catch((e)=>console.log(e))
+}
+module.exports.deleteItems = (req, res) => {
+    const id =req.params.id;
+    
+    todo.findByIdAndDelete(id)
+        .then((data) => {
+            console.log("deleted successfully")
+            res.status(201).json(data)
+        })
+        .catch((e) => console.log(e))
+}
