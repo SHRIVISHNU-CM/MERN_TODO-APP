1b89ac28d1d6da063b43b671e754c6affa81899e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-06 17:28:47 +0530

    input field and Edit and delete button

---
+++ pract/src/Components/Container.jsx
@@ -0,0 +1,21 @@
+
+
+function Hello ({text,Edit,remove}){
+    return(
+        <>
+            <div className="border w-[300px] h-32 bg-[#8739F9] flex justify-center flex-col items-center rounded-lg border-t-4 border-t-[#37B9F1] mt-3 mb-3">
+                <div >
+                    <h2 className="text-white text-[1.25rem] font-bold">{text}</h2>
+                </div>
+                <div className="flex gap-5 relative bottom-[-25px]">
+                    <button onClick={Edit} className="border rounded-md p-1 text-white bg-green-500">Edit</button>
+                    <button onClick={remove} className="border rounded-md p-1 text-white bg-red-500">Delete</button>
+                </div>
+            </div>
+            
+        </>
+    )
+}
+
+
+export default Hello
