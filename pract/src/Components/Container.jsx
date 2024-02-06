bfe42f208be338db3055e345df342cdd6c94264f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-06 21:03:33 +0530

    update and remove

--- pract/src/Components/Container.jsx
+++ pract/src/Components/Container.jsx
@@ -1,6 +1,6 @@
 
 
-function Hello ({text,Edit,remove}){
+function Hello ({text,updatemode,remove}){
     return(
         <>
             <div className="border w-[300px] h-32 bg-[#8739F9] flex justify-center flex-col items-center rounded-lg border-t-4 border-t-[#37B9F1] mt-3 mb-3">
@@ -8,7 +8,7 @@ function Hello ({text,Edit,remove}){
                     <h2 className="text-white text-[1.25rem] font-bold">{text}</h2>
                 </div>
                 <div className="flex gap-5 relative bottom-[-25px]">
-                    <button onClick={Edit} className="border rounded-md p-1 text-white bg-green-500">Edit</button>
+                    <button onClick={updatemode} className="border rounded-md p-1 text-white bg-green-500">Edit</button>
                     <button onClick={remove} className="border rounded-md p-1 text-white bg-red-500">Delete</button>
                 </div>
             </div>
