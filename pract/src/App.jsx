c7d52e782b3fb03fe6eee9768f4e5adb69841c75
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-06 13:40:10 +0530

    adding input field

--- pract/src/App.jsx
+++ pract/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 
 function App() {
@@ -8,6 +8,10 @@ function App() {
     <>
     <div className="text-2xl bg-slate-500 text-center w-full h-min p-3">
         <h3>TODO APP</h3>
+    </div>
+    <div className='flex justify-center mt-3 gap-3'>
+      <input type='text' placeholder='Enter Todo here....' className='outline-none p-1 border-b-2 border-b-slate-500'/>
+      <button className='border p-2 rounded-lg bg-cyan-600'>Add</button>
     </div>
     </>
   )
